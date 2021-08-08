6d7d1599f59af1184a886958fbf2555268b58c3d
hushuilong <shuilonghu@tencent.com>
2021-08-09 00:55:19 +0800

    chore: add example hello-vue

---
+++ examples/hello-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Demo of http-request-mock.</h1>
+
+  <HelloWorld />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style type>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

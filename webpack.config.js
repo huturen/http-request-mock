5f4930453b1b15fdda40b2129d9c3839233ee130
hushuilong <364346488@qq.com>
2021-09-18 16:37:04 +0800

    chore: add a plain html example

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+const externals = {
+  http: 'http',
+  https: 'https',
+  zlib: 'zlib',
+  net: 'net',
+  util: 'util',
+  url: 'url',
+};
+
+let bundle = 'http-mock-request.js';
+let env = 'full';
+for(let i = 0; i < process.argv.length; i++) {
+  if (/^--env=\w+$/.test(process.argv[i])) {
+    env = process.argv[i].split('=')[1];
+  } else if (process.argv[i] === '--env' && process.argv[i+1]) {
+    env = process.argv[i+1];
+  }
+}
+
+if (env === 'pure') {
+  bundle = 'http-mock-request.pure.js';
+  externals['faker/locale/en'] = 'commonjs faker/locale/en';
+}
+
+
+module.exports = {
+  mode: 'production',  // development, production
+  entry: './src/bundle.ts',
+  // devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  // make node modules external
+  externals,
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [],
+  output: {
+    filename: bundle,
+    path: path.resolve(__dirname, 'dist'),
+  },
+};

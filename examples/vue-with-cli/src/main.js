59ae4146593d4cae0ccdf80e1ac32a46325faf83
hushuilong <364346488@qq.com>
2022-03-07 17:41:36 +0800

    chore: delete examples

--- examples/vue-with-cli/src/main.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-/* eslint-enable */
-import { createApp } from 'vue';
-import '../mock/.runtime.js';
-import App from './App.vue';
-
-
-createApp(App).mount('#app')
--- examples/vue-with-cli/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  configureWebpack: {
-  },
-
-  devServer: {
-    disableHostCheck: true,
-  },
-}

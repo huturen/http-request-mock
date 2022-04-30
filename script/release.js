bc3d2812adfcc7c3f9663b282f9cc924db7b927e
hushuilong <364346488@qq.com>
2022-04-30 22:58:41 +0800

    fix: fix release script

--- script/release.js
+++ script/release.js
@@ -34,7 +34,7 @@ if (build.status !== 0) {
 }
 spawnSync('sleep 1', opts);
 
-const publishCmd = `npm publish ${path.resolve(__dirname, '../dist')}`;
+const publishCmd = `cd ${path.resolve(__dirname, '../dist')} && npm publish`;
 console.log('cmd:', publishCmd);
 spawnSync(publishCmd, { ...opts, stdio: 'inherit' });
 

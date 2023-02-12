a48f6574db8cf890fb949b5f6dbede429cee9fb0
shuilonghu <364346488@qq.com>
2023-02-12 18:58:48 +0800

    chore: update runtime.es6.js template

--- tool/tpl/runtime.es6.js
+++ tool/tpl/runtime.es6.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
-let mocker;
 import HttpRequestMock from "__hrm_index_entry__";
+let mocker;
 
 /* __hrf_env_if__ */if (process.env.__hrm_environment_key__ === "__hrm_environment_val__") {
   mocker = HttpRequestMock.setup(__hrm_proxy_server__);
